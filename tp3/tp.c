0e4ef7eba1061927a877e6a575f36184d7b6042f
Zephyr <conzelf@bde.enseeiht.fr>
2019-11-01 12:00:45 +0100

    WIP

--- tp3/tp.c
+++ tp3/tp.c
@@ -1,9 +1,81 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
 #include <info.h>
+#include <segmem.h>
+#include <string.h>
+
+#define c0_idx  1
+#define d0_idx  2
+#define c3_idx  3
+#define d3_idx  4
+
+#define c0_sel  gdt_krn_seg_sel(c0_idx)
+#define d0_sel  gdt_krn_seg_sel(d0_idx)
+#define c3_sel  gdt_krn_seg_sel(c3_idx)
+#define d3_sel  gdt_krn_seg_sel(d3_idx)
+
+seg_desc_t GDT[6];
+
+#define gdt_flat_dsc(_dSc_,_pVl_,_tYp_)                                 \
+   ({                                                                   \
+      (_dSc_)->raw     = 0;                                             \
+      (_dSc_)->limit_1 = 0xffff;                                        \
+      (_dSc_)->limit_2 = 0xf;                                           \
+      (_dSc_)->type    = _tYp_;                                         \
+      (_dSc_)->dpl     = _pVl_;                                         \
+      (_dSc_)->d       = 1;                                             \
+      (_dSc_)->g       = 1;                                             \
+      (_dSc_)->s       = 1;                                             \
+      (_dSc_)->p       = 1;                                             \
+   })
+
+#define c0_dsc(_d) gdt_flat_dsc(_d,0,SEG_DESC_CODE_XR)
+#define d0_dsc(_d) gdt_flat_dsc(_d,0,SEG_DESC_DATA_RW)
+#define c3_dsc(_d) gdt_flat_dsc(_d,3,SEG_DESC_CODE_XR)
+#define d3_dsc(_d) gdt_flat_dsc(_d,3,SEG_DESC_DATA_RW)
 
 extern info_t *info;
 
+void init_gdt()
+{
+   gdt_reg_t gdtr;
+
+   GDT[0].raw = 0ULL;
+
+   c0_dsc( &GDT[c0_idx] );
+   d0_dsc( &GDT[d0_idx] );
+   c3_dsc( &GDT[c3_idx] );
+   d3_dsc( &GDT[d3_idx] );
+
+   gdtr.desc  = GDT;
+   gdtr.limit = sizeof(GDT) - 1;
+   set_gdtr(gdtr);
+
+   set_cs(c0_sel);
+
+   set_ss(d0_sel);
+   set_ds(d0_sel);
+   set_es(d0_sel);
+   set_fs(d0_sel);
+   set_gs(d0_sel);
+}
+
+void userland()
+{
+  asm volatile ("mov %eax, %cr0");
+}
+
 void tp()
 {
+  debug("Before init %d\n");
+  init_gdt();
+  set_ds(d3_sel);
+  set_es(d3_sel);
+  set_fs(d3_sel);
+  set_gs(d3_sel);
+
+  userland();
+
+
+  debug("Yes %d\n");
 }
